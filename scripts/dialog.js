03b46262349965226d87a5950b654fad8d9ad240
Tatyana Vasileva <teanidd@yandex.ru>
2025-06-29 17:23:26 +0300

    первый вариант до исправлений

---
+++ scripts/dialog.js
@@ -0,0 +1,11 @@
+   
+    const openButton = document.querySelector('.button-safe-1');
+    const dialog = document.getElementById('dialog-id');
+    const closeButton = document.getElementById('closeButton');
+          closeButton.addEventListener('click', function() {dialog.close();
+            });
+    openButton.addEventListener('click', function() {
+      dialog.showModal();
+    });
+    
+  
